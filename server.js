f4497269661ded0352107ac6b8d37613846ab06c
L Gabriel Miller <gmiller2007@gmail.com>
2013-03-27 20:25:10 -0400

    Adds Connect to server.js. Routes static files.

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var http = require('http');
-var port = 8080;
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Card Player!!!!");
-}).listen(port);
+var port = 8060;
+var session_key = "p2(236cVb3S#a'25gffDxrR|tb{{bddR31aAz35917";
+var connect = require('connect')
+  , http = require('http');
+var app = connect()
+    .use(connect.favicon())
+    .use(connect.static('static'))
+    .use(connect.directory('static'))
+    .use(connect.cookieParser())
+    .use(connect.session({ secret: session_key }))
+//    .use(connect.logger('logger'))
+//    .use(function(req, res){
+//        res.end('Hello Card Players!\n');
+//    });
+http.createServer(app).listen(port);
 console.log("Server is listening on port "+port+".");
