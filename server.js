73feb2d00b3d9dd36bbcee071157c4ea5009814b
L Gabriel Miller <gmiller2007@gmail.com>
2013-03-29 20:36:12 -0400

    Rewrites server.js to use express and some of the middleware from connect.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var port = 8060;
 var session_key = "p2(236cVb3S#a'25gffDxrR|tb{{bddR31aAz35917";
 var static_dir = "static";
-var connect = require('connect')
-  , http = require('http');
-var connnect_server = connect()
-    .use(connect.favicon('static/img/favicon-16.ico'))
-    .use(connect.logger('dev'))
-    .use(connect.static(static_dir))
-    .use(connect.directory(static_dir))
-    .use(connect.cookieParser())
-    .use(connect.session({ secret: session_key }));
-http.createServer(connect_server).listen(port);
+var http = require('http')
+  , express = require('express');
+var app = express()
+ .use(express.favicon('static/img/favicon-16.ico'))
+ .use(express.logger('dev'))
+ .use(express.static(static_dir))
+ .use(express.directory(static_dir))
+ .use(express.cookieParser())
+ .use(express.session({ secret: session_key }));
+app.listen(port);
 console.log("Server is listening on port "+port+".");
