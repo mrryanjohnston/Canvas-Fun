1230759b8fea7ba46ca333a605ce216b94af38fb
L Gabriel Miller <gmiller2007@gmail.com>
2013-03-29 20:18:16 -0400

    Adds a shitty favicon.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var port = 8060;
 var session_key = "p2(236cVb3S#a'25gffDxrR|tb{{bddR31aAz35917";
+var static_dir = "static";
 var connect = require('connect')
   , http = require('http');
 var app = connect()
-    .use(connect.favicon())
+//    .use(connect.favicon())
+    .use(connect.favicon('static/img/favicon-16.ico'))
     .use(connect.logger('dev'))
-    .use(connect.static('static'))
-    .use(connect.directory('static'))
+    .use(connect.static(static_dir))
+    .use(connect.directory(static_dir))
     .use(connect.cookieParser())
     .use(connect.session({ secret: session_key }));
 http.createServer(app).listen(port);
