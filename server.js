1949e0bc39f6b76b207a4635273028ef108af5ec
L Gabriel Miller <gmiller2007@gmail.com>
2013-03-30 09:09:08 -0400

    Moves settings to a separate file.

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 /**
 * Basic settings
 */
-var project_directory = __dirname;
-var views_directory = project_directory+"/views/";
-var static_directory = project_directory+"/static";
-var favicon_path = static_directory+"/img/favicon-16.ico";
-var port = 8060;
-var session_key = "p2(236cVb3S#a'25gffDxrR|tb{{bddR31aAz35917";
+var settings = require('./settings.js');
 
 /**
 * Initialize node modules
@@ -19,28 +14,29 @@ var http = require('http')
 /**
 * Initialize express and give it middleware
 */
+
 var app = express()
-    .use(express.favicon(favicon_path))
+    .use(express.favicon(settings.favicon_path))
     .use(express.logger('dev'))
-    .use(express.static(static_directory))
+    .use(express.static(settings.static_directory))
     .use(express.cookieParser())
-    .use(express.session({ secret: session_key }));
+    .use(express.session({ secret: settings.session_key }));
 
 /**
 * View settings
 */
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
-app.set('views', views_directory ); // Tells swig where to look for templates
-swig.init({ root: views_directory }); // Tells swig where to look for extended templates
+app.set('views', settings.views_directory ); // Tells swig where to look for templates
+swig.init({ root: settings.views_directory }); // Tells swig where to look for extended templates
 
 /**
 * Load the routes
 */
-var routes = require(project_directory + '/routes.js')(app);
+var routes = require(settings.project_directory + '/routes.js')(app);
 
 /**
 * Start the app
 */
-app.listen(port);
-console.log("Server is listening on port "+port+".");
+app.listen(settings.port);
+console.log("Server is listening on port "+settings.port+".");
