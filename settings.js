242c578171b8365a55a6481609bc9227ff2f271f
L Gabriel Miller <gmiller2007@gmail.com>
2013-03-30 09:09:22 -0400

    Adds settings file.

---
+++ settings.js
@@ -0,0 +1,8 @@
+i = module.exports;
+
+i.project_directory = __dirname;
+i.views_directory = __dirname+"/views";
+i.static_directory = __dirname+"/static";
+i.favicon_path = __dirname+"/img/favicon-16.ico";
+i.port = 8060;
+i.session_key = "p2(236cVb3S#a'25gffDxrR|tb{{bddR31aAz35917";
