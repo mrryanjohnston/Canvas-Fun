b7985eb056d0076851421551e9b3df79b0b06f6c
L Gabriel Miller <gmiller2007@gmail.com>
2013-04-07 20:46:47 -0400

    Makes about page the home page. Makes app throw 404 errors when pages that cannot be accessed when signed in or signed out are accessed at the respective wrong state. Moves play and notifications items to the left side of the menu.

--- routes.js
+++ routes.js
@@ -7,127 +7,149 @@ var debug = function(req){
 };
 
 /***
-* /
+* / - About
 */
 app.get('/', function(req, res) {
-    page_context.page_title = '';
-    res.render('index', {
-        session: req.session,
-        context: page_context
-    });
-    debug(req);
-});
-
-/***
-* /chat
-*/
-app.get('/chat', function(req, res) {
-    page_context.page_title = ': Chat';
-    res.render('chat', {
-        session: req.session,
-        context: page_context
-    });
-    debug(req);
-});
-
-/***
-* /game
-*/
-app.get('/game', function(req, res) {
-    page_context.page_title = ': Game in progress';
-    res.render('game', {
-        session: req.session,
-        context: page_context
-    });
-    debug(req);
-});
-
-/***
-* /login
-*/
-app.get('/login', function(req, res) {
-    page_context.page_title = ': Login';
-    res.render('login', {
-        session: req.session,
-        context: page_context
-    });
-    debug(req);
-});
-
-app.post('/login', function(req, res) {
-    var username = req.body.username
-    ,   password = req.body.password;
-    if( username === "username" && password === "password" ){
-        req.session.username = req.body.username;
-        //req.session.messages.push("<li><p class=\"text-success\">You are signed in.</p></li>"); //This will never be seen.
-        res.redirect('/chat');
-    }else{
-        page_context.page_title = ': Login';
-        req.session.messages.push("<li><p class=\"text-error\">Error: The credentials you provided are not valid.</p></li>");
-        res.render('login', {
+        page_context.page_title = '';
+        res.render('about', {
             session: req.session,
             context: page_context
         });
+    debug(req);
+});
+
+/***
+* /chat - Global chat
+*/
+app.get('/chat', function(req, res) {
+    if (req.session.username){
+        page_context.page_title = ': Chat';
+        res.render('chat', {
+            session: req.session,
+            context: page_context
+        });
+    }else{
+        res.redirect('*');
     }
     debug(req);
 });
 
 /***
-* /logout
+* /game - Playing games
+*/
+app.get('/game', function(req, res) {
+    if (req.session.username){
+        page_context.page_title = ': Game in progress';
+        res.render('game', {
+            session: req.session,
+            context: page_context
+        });
+    }else{
+        res.redirect('*');
+    }
+    debug(req);
+});
+
+/***
+* /login - Login
+*/
+app.get('/login', function(req, res) {
+    if (!req.session.username){
+        page_context.page_title = ': Login';
+        res.render('login', {
+            session: req.session,
+            context: page_context
+        });
+    }else{
+        res.redirect('*');
+    }
+    debug(req);
+});
+
+app.post('/login', function(req, res) {
+    if(!req.session.username){
+        var username = req.body.username
+        ,   password = req.body.password;
+        if( username === "username" && password === "password" ){
+            req.session.username = req.body.username;
+            //req.session.messages.push("<li><p class=\"text-success\">You are signed in.</p></li>"); //This will never be seen.
+            res.redirect('/chat');
+        }else{
+            page_context.page_title = ': Login';
+            req.session.messages.push("<li><p class=\"text-error\">Error: The credentials you provided are not valid.</p></li>");
+            res.render('login', {
+                session: req.session,
+                context: page_context
+            });
+        }
+    }else{
+        res.redirect('*');
+    }
+    debug(req);
+});
+
+/***
+* /logout - Logout
 */
 app.get('/logout', function(req, res) {
-    req.session.destroy();
-    page_context.page_title = ': Logout';
-    //req.session.messages.push("<li><p class=\"text-success\">You are signed out.</p></li>"); //This will never be seen.
-    res.redirect('/login');
+    if (req.session.username){
+        req.session.destroy();
+        page_context.page_title = ': Logout';
+        //req.session.messages.push("<li><p class=\"text-success\">You are signed out.</p></li>"); //This will never be seen.
+        res.redirect('/login');
+    }else{
+        res.redirect('*');
+    }
     debug(req);
 });
 
 /***
-* /signup
+* /signup - Registration
 */
 app.get('/signup', function(req, res) {
-    page_context.page_title = ': Sign Up';
-    res.render('signup', {
-        session: req.session,
-        context: page_context
-    });
+    if (!req.session.username){
+        page_context.page_title = ': Sign Up';
+        res.render('signup', {
+            session: req.session,
+            context: page_context
+        });
+    }else{
+        res.redirect('*');
+    }
     debug(req);
 });
 
 /***
-* /user
+* /user - User Dashboard
 */
 app.get('/user', function(req, res, next) {
-    page_context.page_title = ': User Settings';
-    res.render('user', {
-        session: req.session,
-        context: page_context
-    });
+    if (req.session.username){
+        page_context.page_title = ': User Dashboard';
+        res.render('user', {
+            session: req.session,
+            context: page_context
+        });
+    }else{
+        res.redirect('*');
+    }
     debug(req);
 });
 
 /***
-* /about
+* /404 and /* - 404 error
 */
-app.get('/about', function(req, res, next) {
-    page_context.page_title = ': About';
-    res.render('about', {
-        session: req.session,
-        context: page_context
-    });
-    debug(req);
-});
-
-/***
-* 404 error
-*/
-app.get('*', function(req, res, next) {
+app.get('/404', function(req, res, next) {
     page_context.page_title = ': Error';
     res.render('404', {
         session: req.session,
         context: page_context
     });
+    debug(req);
+});
+
+app.get('*', function(req, res, next) {
+    page_context.page_title = ': Error';
+    res.redirect('/404');
     debug(req);
 });
 
