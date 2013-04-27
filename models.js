ae342925f5854697ba672db2588953b3fdd4a8c7
L Gabriel Miller <gmiller2007@gmail.com>
2013-04-26 20:55:06 -0400

    Removes attempted autoincrementing integer id. Removes unnecessary/silly information from model/db.

--- models.js
+++ models.js
@@ -7,7 +7,6 @@ module.exports = function models_function(mongoose, settings) {
             ,   salt : String
             ,   username : String
             ,   date_signup : { type: Date, default: Date.now }
-            ,   date_login : Date
             ,   record_games_won : { type : Number, default: 0 }
             ,   record_games_lost : { type : Number, default : 0 }
             ,   avatar_url : { type : String, default : '' }
@@ -19,14 +18,5 @@ module.exports = function models_function(mongoose, settings) {
     // http://stackoverflow.com/questions/14454271/auto-increment-document-number-in-mongo-mongoose
     //console.log(models.user.pre);
     //console.log(mongoose);
-    mongoose.modelSchemas.user.pre('save', function pre_save_call(next) {
-        console.log("OK!");
-        var doc = this;
-        mongoose.models.user.findByIdAndUpdate( settings.user_id, { $inc: { nextSeqNumber: 1 } }, function pre_save_call_2(error, id) {
-            if (error) next(error);
-            doc.number = mongoose.models.user.nextSeqNumber - 1;
-            next();
-        });
-    });
     return models;
 }
