5d6e3616ab776cb9530a6e3a956ffb31e50b3cc0
L. Gabriel Miller <gmiller2007@gmail.com>
2013-11-18 22:16:00 -0500

    Updates signup form to not look like balls. Adds a little bit of content to user controller. Puts in a basic clientside user authentication. Removes excess spaces from css.

--- static/js/angular/app.js
+++ static/js/angular/app.js
@@ -19,15 +19,32 @@ cardsApp.directive('focusMe', function($timeout) {
 cardsApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.when('/', {
+            authentication: false,
             templateUrl: 'partials/about.html',
             controller: 'user'
         }).
         when('/signup', {
+            authentication: false,
+            templateUrl: 'partials/signup.html',
+            controller: 'user'
+        }).
+        when('/signupp', {
+            authentication: true,
             templateUrl: 'partials/signup.html',
             controller: 'user'
         }).
         otherwise({
             redirectTo: '/'
-        });
+        })
+        /*.
+        run($scope.$on("$routeChangeStart", function(event, next, current){
+            var authRequired = $route.current && $route.current.$route && $route.current.$route.authentication;
+            if (authRequired && !authentication.loggedin === false){
+                console.log("Auth required for the requested URL.");
+                var currentUrl = $location.url();
+                //$location.url("/#/signin?redirect_url=" + encodeURIComponent(currentUrl));
+            }
+        }));
+        */;
     }
 ]);
--- static/js/angular/controllers.js
+++ static/js/angular/controllers.js
@@ -1,9 +1,31 @@
 var cardsControllers = angular.module('cardsControllers', []);
 
 /* User controller */
-cardsControllers.controller('user', ['$scope', '$routeParams',
-    function user($scope,$routeParams){
-        
+cardsControllers.controller('user', ['$scope', '$routeParams', '$location', function user($scope,$routeParams,$location,$cookies){ 
+
+    $scope.loggedIn = false;
+
+    $scope.login = function(){
+        $scope.loggedIn = true;
+    }
+
+    $scope.logout = function(){
+        $scope.loggedIn = false;
+    }
+
+    $scope.signup = function(){
+        // Send ajax request to /signup
+        // if 200 then save the cookie
+        // $cookies[responseKey] = response;
+    }
+
+    /* Redirect unauthenticated users to a login form */
+    $scope.$watch(function() { return $location.path(); }, function(newValue, oldValue){
+        if($scope.loggedIn === false && (!(newValue in {'/signup':true,'/':true}))){  
+            $location.path('/');
+        }
+    });
+
 }]);
 
 /* Play controller */
