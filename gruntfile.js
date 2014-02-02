8f840ac6a633e8ffa61ba0e803499a8c44f9cbd2
L Gabriel Miller <gmiller2007@gmail.com>
2014-02-01 21:12:49 -0500

    Adds bower dependencies for kineticjs, holderjs, and socket.io-client. Adds grunt copy tasks to copy bower dependencies and bootstrap javascripts to webroot directories for angular, jquery, kineticjs, holderjs, socket.io-client, and bootstrap javascripts.

--- gruntfile.js
+++ gruntfile.js
@@ -10,6 +10,48 @@ module.exports = function(grunt) {
                         expand: true,
                         flatten: true,
                         filter: 'isFile'
+                    },
+                    {
+                        src:["bower_components/angular/angular.js","bower_components/angular/angular.min.js","bower_components/angular/angular.min.js.map"],
+                        dest: "static/js/lib/angular",
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        src:["bower_components/jquery/jquery.js","bower_components/jquery/jquery.min.js","bower_components/jquery/jquery.min.map"],
+                        dest: "static/js/lib/jquery",
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        src:["bower_components/kineticjs/kinetic.js","bower_components/kineticjs/kinetic.min.js"],
+                        dest: "static/js/lib/kinetic",
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        src:"bower_components/holderjs/holder.js",
+                        dest: "static/js/lib/holder",
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        src:["bower_components/socket.io-client/dist/socket.io.js","bower_components/socket.io-client/dist/socket.io.min.js"],
+                        dest: "static/js/lib/socket.io",
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
+                    },
+                    {
+                        src: "scss/javascripts/bootstrap/*",
+                        dest: "static/js/lib/bootstrap",
+                        expand: true,
+                        flatten: true,
+                        filter: 'isFile'
                     }
                 ]
             }
